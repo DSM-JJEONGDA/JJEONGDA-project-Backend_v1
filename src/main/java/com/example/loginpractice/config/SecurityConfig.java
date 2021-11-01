fcd7daf76d0487e3f0ad79a3d845d12a58d53350
이정윤 <dlwjddbs14@naver.com>
2021-11-01 20:09:38 +0900

    [UPDATE] config 설정

--- src/main/java/com/example/loginpractice/config/SecurityConfig.java
+++ src/main/java/com/example/loginpractice/config/SecurityConfig.java
@@ -2,30 +2,46 @@ package com.example.loginpractice.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenProvider jwtTokenProvider;
-    
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+    // authenticationManager를 Bean 등록합니다.
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .httpBasic()
+                .httpBasic().disable() //rest api 만을 고려하여 기본설정은 해제 하겠습니다.
+                .csrf().disable() //csrf 보안 토큰 disable 처리.
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //토큰기반 인증이므로 세션 역시 사용하지 않음.
                 .and()
-                .authorizeRequests()
+                .authorizeRequests() //요청에 대한 사용권한 체크
                 .antMatchers("/admin/**").hasRole("ADMIN") //인증요구
                 .antMatchers("/user/**").hasRole("USER") //인증요구
-                .antMatchers("/**").permitAll();
+                .antMatchers("/**").permitAll() //그 외 나머지 요청은 누구나 접근 가능
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class);
+                //JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 전에 넣는다
     }
 }
