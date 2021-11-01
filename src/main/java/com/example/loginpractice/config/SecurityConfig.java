001dc62e4796983678fd5398889bfa9cf8e09b1c
이정윤 <dlwjddbs14@naver.com>
2021-11-01 20:02:05 +0900

    [UPDATE] 암호화 Bean 등록

--- src/main/java/com/example/loginpractice/config/SecurityConfig.java
+++ src/main/java/com/example/loginpractice/config/SecurityConfig.java
@@ -1,13 +1,23 @@
 package com.example.loginpractice.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     protected void configure(HttpSecurity http) throws Exception {
         http
