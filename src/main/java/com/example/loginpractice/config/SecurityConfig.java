35357fdadbef694c403747282ac3301f3a822661
이정윤 <dlwjddbs14@naver.com>
2021-11-01 11:20:25 +0900

    [ADD]

---
+++ src/main/java/com/example/loginpractice/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.loginpractice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN") //인증요구
+                .antMatchers("/user/**").hasRole("USER") //인증요구
+                .antMatchers("/**").permitAll();
+    }
+}
