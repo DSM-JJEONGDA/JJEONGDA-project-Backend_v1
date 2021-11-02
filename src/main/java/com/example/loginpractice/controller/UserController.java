0251920ba657faaa69c8618dd9bb57afb57fb43c
이정윤 <dlwjddbs14@naver.com>
2021-11-02 23:37:23 +0900

    [UPDATE] 회원가입, 로그인 로직

--- src/main/java/com/example/loginpractice/controller/UserController.java
+++ src/main/java/com/example/loginpractice/controller/UserController.java
@@ -1,2 +1,45 @@
-package com.example.loginpractice.controller;public class UserController {
+package com.example.loginpractice.controller;
+
+import com.example.loginpractice.config.JwtTokenProvider;
+import com.example.loginpractice.entity.User;
+import com.example.loginpractice.entity.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserRepository userRepository;
+
+    // 회원가입
+    @PostMapping("/join")
+    public Long join(@RequestBody Map<String, String> user){
+        return userRepository.save(User.builder()
+                .email(user.get("email"))
+                .password(passwordEncoder.encode(user.get("password")))
+                .roles(Collections.singletonList("ROLE_USER")) // 최초 가입시 USER 로 설정
+                .build()).getId();
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody Map<String, String> user) {
+        User member = userRepository.findByEmail(user.get("email"))
+                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
+        if(!passwordEncoder.matches(user.get("password"), member.getPassword())) {
+            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+        }
+        return jwtTokenProvider.createToken(member.getUsername(), member.getRoles());
+    }
 }
