f6bead03ce0e3b6698aa489acc302b62ca88a41a
이정윤 <dlwjddbs14@naver.com>
2021-11-06 16:14:58 +0900

    [ADD] DiaryRepository

---
+++ src/main/java/com/example/loginpractice/entity/DiaryRepository.java
@@ -0,0 +1,6 @@
+package com.example.loginpractice.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiaryRepository extends JpaRepository<DiaryEntity, Long> {
+}
