363dbc7aa6e24b8a7f3a0934a8805bc4494349f8
이정윤 <dlwjddbs14@naver.com>
2021-11-06 16:29:06 +0900

    [UPDATE] 수정로직 추가

--- src/main/java/com/example/loginpractice/controller/DiaryController.java
+++ src/main/java/com/example/loginpractice/controller/DiaryController.java
@@ -4,9 +4,7 @@ import com.example.loginpractice.dto.DiaryRequest;
 import com.example.loginpractice.entity.DiaryEntity;
 import com.example.loginpractice.service.DiaryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,13 +15,19 @@ public class DiaryController {
     private final DiaryService diaryService;
 
     //글쓰기
-    @PostMapping
+    @PostMapping("/write")
     public String create(DiaryRequest request){
         return diaryService.create(request);
     }
 
+    //리스트
     @GetMapping("/list")
     public List<DiaryEntity> getAllDiary(){
         return diaryService.getAllDiary();
+    }
+
+    @PatchMapping("/update/{diaryPk}")
+    public String update(@PathVariable Long diaryPk, @RequestBody DiaryRequest request) {
+        return diaryService.update(diaryPk, request);
     }
 }
