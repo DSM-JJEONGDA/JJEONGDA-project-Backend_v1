b8f7b60efcb893d7a134993e8953438f8c50f958
이정윤 <dlwjddbs14@naver.com>
2021-11-06 16:18:50 +0900

    [UPDATE] controller 글쓰기 로직 추가

--- src/main/java/com/example/loginpractice/controller/DiaryController.java
+++ src/main/java/com/example/loginpractice/controller/DiaryController.java
@@ -1,9 +1,11 @@
 package com.example.loginpractice.controller;
 
+import com.example.loginpractice.dto.DiaryRequest;
 import com.example.loginpractice.entity.DiaryEntity;
 import com.example.loginpractice.service.DiaryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,6 +15,12 @@ import java.util.List;
 public class DiaryController {
 
     private final DiaryService diaryService;
+
+    //글쓰기
+    @PostMapping
+    public String create(DiaryRequest request){
+        return diaryService.create(request);
+    }
 
     @GetMapping("/list")
     public List<DiaryEntity> getAllDiary(){
