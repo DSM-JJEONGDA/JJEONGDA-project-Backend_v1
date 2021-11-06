bfcbef7f210faf3af1415e7fa13568b683c0e9ff
이정윤 <dlwjddbs14@naver.com>
2021-11-06 16:15:33 +0900

    [ADD] DiaryService

---
+++ src/main/java/com/example/loginpractice/service/DiaryService.java
@@ -0,0 +1,25 @@
+package com.example.loginpractice.service;
+
+import com.example.loginpractice.dto.DiaryRequest;
+import com.example.loginpractice.entity.DiaryEntity;
+import com.example.loginpractice.entity.DiaryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DiaryService {
+
+    private final DiaryRepository diaryRepository;
+
+    //글쓰기
+    public String create(DiaryRequest request){
+        DiaryEntity diaryEntity = DiaryEntity.builder()
+                .title(request.getTitle())
+                .weather(request.getWeather())
+                .contents(request.getContents())
+                .build();
+        diaryRepository.save(diaryEntity);
+        return "글쓰기 성공";
+    }
+}
