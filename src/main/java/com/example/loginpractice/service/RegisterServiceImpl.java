ccf37d3d2b85f57e9bf8a69720fe41e0c76008ba
이정윤 <dlwjddbs14@naver.com>
2021-11-08 14:09:19 +0900

    [UPDATE] 회원가입 서비스 impl 추가

---
+++ src/main/java/com/example/loginpractice/service/RegisterServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.loginpractice.service;
+
+import com.example.loginpractice.entity.User;
+import com.example.loginpractice.entity.UserRepository;
+import com.example.loginpractice.exception.AlreadyUserExistException;
+import com.example.loginpractice.payload.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RegisterServiceImpl implements RegisterService{
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public String register(RegisterRequest request) {
+        try {
+            userRepository.save(
+                    User.builder()
+                            .email(request.getEmail())
+                            .password(passwordEncoder.encode(request.getPassword()))
+                            .build()
+            );
+            return "회원가입 성공";
+        } catch (Exception e) {
+            throw new AlreadyUserExistException();
+        }
+    }
+}
