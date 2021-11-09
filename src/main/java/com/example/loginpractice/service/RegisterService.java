2138a11937e6fd06f4701b1a4b563e05ae4225c7
이정윤 <dlwjddbs14@naver.com>
2021-11-09 16:57:17 +0900

    [DELETE] 코드 갈아엎기

--- src/main/java/com/example/loginpractice/service/RegisterService.java
+++
@@ -1,7 +0,0 @@
-package com.example.loginpractice.service;
-
-import com.example.loginpractice.payload.RegisterRequest;
-
-public interface RegisterService {
-    String register(RegisterRequest request);
-}
