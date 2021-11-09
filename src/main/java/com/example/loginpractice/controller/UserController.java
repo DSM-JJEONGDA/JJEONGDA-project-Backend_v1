4cc0f6432141dd31a3f912be1500fcc7d0c0f801
이정윤 <dlwjddbs14@naver.com>
2021-11-09 19:23:43 +0900

    [UPDATE] 로직 수정

--- src/main/java/com/example/loginpractice/controller/UserController.java
+++ src/main/java/com/example/loginpractice/controller/UserController.java
@@ -1,18 +1,20 @@
 package com.example.loginpractice.controller;
 
-import com.example.loginpractice.jwt.AccessTokenResponseDto;
+import com.example.loginpractice.entity.User;
 import com.example.loginpractice.jwt.JwtTokenProvider;
 import com.example.loginpractice.entity.UserRepository;
-import com.example.loginpractice.payload.LoginRequest;
 import com.example.loginpractice.payload.RegisterRequest;
 import com.example.loginpractice.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -25,33 +27,19 @@ public class UserController {
 
     //회원가입
     @PostMapping("/register")
-    public String register(@RequestBody @Valid RegisterRequest request){
+    @ResponseStatus(HttpStatus.CREATED)
+    public String register(@RequestBody @Valid RegisterRequest request) {
         return registerService.register(request);
     }
 
     //로그인
-    /*@PostMapping("/login")
+    @PostMapping("/login")
     public String login(@RequestBody Map<String, String> user) {
         User member = userRepository.findByEmail(user.get("email"))
                 .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
-        if(!passwordEncoder.matches(user.get("password"), member.getPassword())) {
+        if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {
             throw new IllegalArgumentException("잘못된 비밀번호입니다.");
         }
         return jwtTokenProvider.createToken(member.getUsername(), member.getRoles());
-    }*/
-/*
-    @PostMapping("/login")
-    public String login(@RequestBody LoginRequest request){
-        User user = userRepository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
-        if (!passwordEncoder.matches(passwordEncoder.encode(request.getPassword()), user.getPassword())) {
-            throw new IllegalArgumentException("잘못된 비밀번호 입니다.");
-        }
-        return jwtTokenProvider.createToken(user.getUsername(), user.getRoles());
-    }
-    */
-    @PostMapping("/login")
-    public AccessTokenResponseDto login(@RequestBody LoginRequest request){
-        return registerService.login(request);
     }
 }
