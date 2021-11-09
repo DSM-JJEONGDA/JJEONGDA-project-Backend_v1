d6b304cb7189084fad1f78c06c39d61e73483bcb
이정윤 <dlwjddbs14@naver.com>
2021-11-09 19:22:47 +0900

    [UPDATE] auth 서비스 interface

--- src/main/java/com/example/loginpractice/service/AuthService.java
+++ src/main/java/com/example/loginpractice/service/AuthService.java
@@ -1,10 +1,7 @@
 package com.example.loginpractice.service;
 
-import com.example.loginpractice.jwt.AccessTokenResponseDto;
-import com.example.loginpractice.payload.LoginRequest;
 import com.example.loginpractice.payload.RegisterRequest;
 
 public interface AuthService {
     String register(RegisterRequest request);
-    AccessTokenResponseDto login(LoginRequest request);
 }
