0d7c1fe9fa23429860bc2f23bfabfd8dd2fb7801
이정윤 <dlwjddbs14@naver.com>
2021-11-11 14:42:26 +0900

    [UPDATE] redis 코드 수정

--- src/main/java/com/example/loginpractice/config/RedisConfig.java
+++ src/main/java/com/example/loginpractice/config/RedisConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -15,15 +16,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableRedisRepositories
 public class RedisConfig {
 
-    @Value("&{sprig.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.redis.port}")
-    private int redisPort;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory(redisHost, redisPort);
+        return new LettuceConnectionFactory("localhost", 6379);
     }
 
     @Bean
