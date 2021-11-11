00e45169e9c543f88f9dbfa77b97b6458727605f
이정윤 <dlwjddbs14@naver.com>
2021-11-11 14:40:51 +0900

    [ADD] email인증 코드 요청 추가

---
+++ src/main/java/com/example/loginpractice/payload/request/CodeRequest.java
@@ -0,0 +1,21 @@
+package com.example.loginpractice.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CodeRequest {
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String code;
+}
