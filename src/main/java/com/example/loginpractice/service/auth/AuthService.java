ae40270c978cfe3c204c695db1ea4c8d7a2cb09d
이정윤 <dlwjddbs14@naver.com>
2021-11-12 22:55:59 +0900

    [UPDATE] 회원가입 로직 수정

--- src/main/java/com/example/loginpractice/service/auth/AuthService.java
+++ src/main/java/com/example/loginpractice/service/auth/AuthService.java
@@ -1,8 +1,11 @@
-package com.example.loginpractice.service;
+package com.example.loginpractice.service.auth;
 
+import com.example.loginpractice.payload.request.EmailRequest;
+import com.example.loginpractice.payload.request.EmailVerifiedRequest;
 import com.example.loginpractice.payload.request.RegisterRequest;
 
 public interface AuthService {
     String register(RegisterRequest request);
-    void exceute(String email);
+    void sendEmail(EmailRequest request);
+    void verifyAccount(EmailVerifiedRequest request);
 }
