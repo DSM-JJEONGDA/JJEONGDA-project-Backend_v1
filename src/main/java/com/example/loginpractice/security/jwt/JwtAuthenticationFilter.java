774d69bda34625dc5cc15073693890854aaafc07
이정윤 <dlwjddbs14@naver.com>
2021-11-12 22:29:38 +0900

    [UPDATE] 토큰 설정

--- src/main/java/com/example/loginpractice/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/example/loginpractice/security/jwt/JwtAuthenticationFilter.java
@@ -1,28 +1,27 @@
-package com.example.loginpractice.jwt;
+package com.example.loginpractice.security.jwt;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends GenericFilterBean {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    public void doFilter(ServletRequest request,
-                         ServletResponse response,
-                         FilterChain chain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain) throws IOException, ServletException {
         // 헤더에서 JWT를 받아옵니다.
-        String token = jwtTokenProvider.resolveToken((HttpServletRequest) request);
+        String token = jwtTokenProvider.resolveToken(request);
         //유효한 토큰인지 확인합니다.
         if(token != null && jwtTokenProvider.validateToken(token)){
             // 토큰이 유효하면 토큰으로 부터 유저 정보를 받아옵니다.
