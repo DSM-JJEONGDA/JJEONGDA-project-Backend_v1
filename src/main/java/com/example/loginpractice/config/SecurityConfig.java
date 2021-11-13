0de0b6d474fa5318e73ef967a4064ea28f70b7df
이정윤 <dlwjddbs14@naver.com>
2021-11-13 23:12:43 +0900

    [UPDATE] security 설정

--- src/main/java/com/example/loginpractice/config/SecurityConfig.java
+++ src/main/java/com/example/loginpractice/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.example.loginpractice.config;
 
-import com.example.loginpractice.jwt.JwtAuthenticationFilter;
-import com.example.loginpractice.jwt.JwtTokenProvider;
+import com.example.loginpractice.security.jwt.JwtAuthenticationFilter;
+import com.example.loginpractice.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
