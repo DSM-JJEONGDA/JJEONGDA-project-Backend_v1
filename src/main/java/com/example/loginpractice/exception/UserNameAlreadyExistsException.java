572cdbee16fedaef7adf9b97967778eacf787385
이정윤 <dlwjddbs14@naver.com>
2021-11-13 19:50:07 +0900

    [UPDATE] Exception 설정

--- src/main/java/com/example/loginpractice/exception/UserNameAlreadyExistsException.java
+++ src/main/java/com/example/loginpractice/exception/UserNameAlreadyExistsException.java
@@ -1,2 +1,10 @@
-package com.example.loginpractice.exception;public class UserNameAlreadyExistsException {
+package com.example.loginpractice.exception;
+
+import com.example.loginpractice.error.ErrorCode;
+import com.example.loginpractice.error.exception.BusinessException;
+
+public class UserNameAlreadyExistsException extends BusinessException {
+    public UserNameAlreadyExistsException(){
+        super(ErrorCode.MEMBER_NAME_ALREADY_EXISTS);
+    }
 }
