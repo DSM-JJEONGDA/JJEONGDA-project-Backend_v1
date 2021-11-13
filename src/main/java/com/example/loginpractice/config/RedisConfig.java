687c319266c471948a8c4843697c314780401829
이정윤 <dlwjddbs14@naver.com>
2021-11-13 23:14:15 +0900

    [UPDATE] redis config 설정

--- src/main/java/com/example/loginpractice/config/RedisConfig.java
+++ src/main/java/com/example/loginpractice/config/RedisConfig.java
@@ -1,32 +1,36 @@
 package com.example.loginpractice.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@RequiredArgsConstructor
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
 
+    @Value("${spring.redis.host}")
+    private String REDIS_HOST;
+
+    @Value("${spring.redis.port}")
+    private int REDIS_PORT;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory(){
-        return new LettuceConnectionFactory("localhost", 6379);
+        RedisStandaloneConfiguration redisConfig =
+                new RedisStandaloneConfiguration(REDIS_HOST, REDIS_PORT);
+
+        return new LettuceConnectionFactory(redisConfig);
     }
 
     @Bean
     public RedisTemplate<?, ?> redisTemplate(){
         RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
         return redisTemplate;
     }
 }
