8bc890ab84027bd2868a2b5d9855371c3fe78966
이정윤 <dlwjddbs14@naver.com>
2021-11-15 14:23:48 +0900

    [UPDATE] auth 서비스

--- src/main/java/com/example/loginpractice/service/auth/AuthService.java
+++ src/main/java/com/example/loginpractice/service/auth/AuthService.java
@@ -2,10 +2,14 @@ package com.example.loginpractice.service.auth;
 
 import com.example.loginpractice.payload.request.EmailRequest;
 import com.example.loginpractice.payload.request.EmailVerifiedRequest;
+import com.example.loginpractice.payload.request.LoginRequest;
 import com.example.loginpractice.payload.request.RegisterRequest;
+import com.example.loginpractice.payload.response.TokenResponse;
 
 public interface AuthService {
-    String register(RegisterRequest request);
+    void register(RegisterRequest request);
     void sendEmail(EmailRequest request);
     void verifyAccount(EmailVerifiedRequest request);
+    TokenResponse login(LoginRequest request);
+    TokenResponse reissue(String token);
 }
