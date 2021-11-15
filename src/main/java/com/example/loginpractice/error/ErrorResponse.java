59629865c713d4e237c3f6a8b326a07c59f9e2da
이정윤 <dlwjddbs14@naver.com>
2021-11-15 14:24:20 +0900

    [UPDATE] 에러 응답

--- src/main/java/com/example/loginpractice/error/ErrorResponse.java
+++ src/main/java/com/example/loginpractice/error/ErrorResponse.java
@@ -1,10 +1,18 @@
 package com.example.loginpractice.error;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
+@ToString
 public class ErrorResponse {
-    private String message;
+    private final String message;
+
+    public String convertToJson(Object object) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(object);
+    }
 }
