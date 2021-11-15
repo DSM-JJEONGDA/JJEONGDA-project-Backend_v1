e135963c55b62e69e2a49029da7130e1abd434a9
이정윤 <dlwjddbs14@naver.com>
2021-11-15 14:26:50 +0900

    [UPDATE] 로그인 request

--- src/main/java/com/example/loginpractice/payload/request/LoginRequest.java
+++ src/main/java/com/example/loginpractice/payload/request/LoginRequest.java
@@ -1,2 +1,19 @@
-package com.example.loginpractice.payload.request;public class LoginRequest {
+package com.example.loginpractice.payload.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+public class LoginRequest {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
 }
