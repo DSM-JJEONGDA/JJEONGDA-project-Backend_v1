e6d3593eb4fd880adf662af772459110c9e365ec
이정윤 <dlwjddbs14@naver.com>
2021-11-15 14:22:56 +0900

    [UPDATE] Exception 추가

--- src/main/java/com/example/loginpractice/exception/UserNotFoundException.java
+++ src/main/java/com/example/loginpractice/exception/UserNotFoundException.java
@@ -1,2 +1,11 @@
-package com.example.loginpractice.exception;public class UserNotFoundException {
+package com.example.loginpractice.exception;
+
+import com.example.loginpractice.error.ErrorCode;
+import com.example.loginpractice.error.exception.BusinessException;
+
+public class UserNotFoundException extends BusinessException {
+
+    public UserNotFoundException(){
+        super(ErrorCode.USER_NOT_FOUND);
+    }
 }
