9947a404a084fdb88fc04ccea076b16c4c948a98
이정윤 <dlwjddbs14@naver.com>
2021-11-15 14:27:29 +0900

    [UPDATE] controller 로직 수정

--- src/main/java/com/example/loginpractice/controller/UserController.java
+++ src/main/java/com/example/loginpractice/controller/UserController.java
@@ -1,56 +1,49 @@
 package com.example.loginpractice.controller;
 
-import com.example.loginpractice.entity.refreshToken.RefreshTokenRepository;
-import com.example.loginpractice.entity.user.User;
-import com.example.loginpractice.entity.user.UserRepository;
 import com.example.loginpractice.payload.request.EmailRequest;
 import com.example.loginpractice.payload.request.EmailVerifiedRequest;
-import com.example.loginpractice.security.jwt.JwtTokenProvider;
+import com.example.loginpractice.payload.request.LoginRequest;
+import com.example.loginpractice.payload.response.TokenResponse;
 import com.example.loginpractice.payload.request.RegisterRequest;
 import com.example.loginpractice.service.auth.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
 public class UserController {
 
-    private final PasswordEncoder passwordEncoder;
-    private final JwtTokenProvider jwtTokenProvider;
-    private final UserRepository userRepository;
     private final AuthService authService;
-    private final RefreshTokenRepository refreshTokenRepository;
 
+    //email 전송
     @PostMapping("/send")
-    public void sendEmail(@Valid @RequestBody EmailRequest request){
+    public void sendEmail(@Valid @RequestBody EmailRequest request) {
         authService.sendEmail(request);
     }
 
+    //email 인증
     @PostMapping("/email")
-    public void verifyAccount(@Valid @RequestBody EmailVerifiedRequest request){
+    public void verifyAccount(@Valid @RequestBody EmailVerifiedRequest request) {
         authService.verifyAccount(request);
     }
 
     //회원가입
     @PostMapping("/register")
-    @ResponseStatus(HttpStatus.CREATED)
     public void register(@RequestBody @Valid RegisterRequest request) {
         authService.register(request);
     }
 
     //로그인
     @PostMapping("/login")
-    public String login(@RequestBody Map<String, String> user) {
-        User member = userRepository.findByEmail(user.get("email"))
-                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
-        if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {
-            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
-        }
-        return jwtTokenProvider.createJwtAccessToken(member.getUsername());
+    public TokenResponse login(@RequestBody @Valid LoginRequest request) {
+        return authService.login(request);
+    }
+
+    //토큰 재발급
+    @PutMapping("/reissue")
+    public TokenResponse reissue(@RequestHeader(name = "X-REFRESH-TOKEN") String token) {
+        return authService.reissue(token);
     }
 }
