8b9e78461bad270836920c782b42adddecf65002
이정윤 <dlwjddbs14@naver.com>
2021-11-15 14:25:12 +0900

    [UPDATE] Exception 추가

--- src/main/java/com/example/loginpractice/exception/ExpiredAccessTokenException.java
+++ src/main/java/com/example/loginpractice/exception/ExpiredAccessTokenException.java
@@ -1,2 +1,10 @@
-package com.example.loginpractice.exception;public class ExpiredAccessTokenException {
+package com.example.loginpractice.exception;
+
+import com.example.loginpractice.error.ErrorCode;
+import com.example.loginpractice.error.exception.BusinessException;
+
+public class ExpiredAccessTokenException extends BusinessException {
+    public ExpiredAccessTokenException(){
+        super(ErrorCode.EXPIRED_ACCESS_TOKEN);
+    }
 }
