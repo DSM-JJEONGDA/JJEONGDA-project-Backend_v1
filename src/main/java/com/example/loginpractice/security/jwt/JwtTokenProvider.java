ecb8ec643d66ac19f06aacbda7159acf3758b517
이정윤 <dlwjddbs14@naver.com>
2021-11-15 14:11:41 +0900

    [UPDATE] jwt 토큰 설정

--- src/main/java/com/example/loginpractice/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/loginpractice/security/jwt/JwtTokenProvider.java
@@ -1,5 +1,6 @@
 package com.example.loginpractice.security.jwt;
 
+import com.example.loginpractice.exception.ExpiredAccessTokenException;
 import com.example.loginpractice.exception.ExpiredRefreshTokenException;
 import com.example.loginpractice.exception.IncorrectTokenException;
 import com.example.loginpractice.exception.InvalidTokenException;
@@ -11,19 +12,24 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
-import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Date;
-import java.util.List;
 
-@Component
 @RequiredArgsConstructor
+@Component
 public class JwtTokenProvider {
 
-    @Value("${jwt.secret}")
-    private String secretKey;
+    private String SECRET_KEY;
+
+    @Value("${jwt.header}")
+    private String HEADER;
+
+    @Value("${jwt.prefix}")
+    private String PREFIX;
 
     //토큰 유효기간
     private long ACCESS_TOKEN_VALID_TIME = 30 * 60 * 1000L; //30분
@@ -31,10 +37,9 @@ public class JwtTokenProvider {
 
     private final UserDetailsService userDetailsService;
 
-    //객체 초기화, secretKey를 Base64로 인코딩한다.
-    @PostConstruct
-    protected void init(){
-        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    @Value("${jwt.secret}")
+    protected String setSecretKey(){
+        return Base64.getEncoder().encodeToString(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
     }
 
     public String createJwtAccessToken(String username) {
@@ -44,7 +49,7 @@ public class JwtTokenProvider {
                 .claim("type", "access")
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_VALID_TIME))
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
 
@@ -55,40 +60,54 @@ public class JwtTokenProvider {
                 .claim("type", "refresh")
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + REFRESH_TOKEN_VALID_TIME))
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
     // JWT 토큰 에서 인증 정보 조회
     public Authentication getAuthentication(String token){
-        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserPk(token));
+        UserDetails userDetails = userDetailsService.loadUserByUsername(getUsername(token).getSubject());
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
     //토큰에서 회원 정보 추출
-    public String getUserPk(String token){
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    public Claims getUsername(String token){
+        try{
+            return Jwts.parser()
+                    .setSigningKey(SECRET_KEY)
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (MalformedJwtException | UnsupportedJwtException e) {
+            throw new IncorrectTokenException();
+        } catch (ExpiredJwtException e) {
+            throw new ExpiredAccessTokenException();
+        } catch (Exception e) {
+            throw new InvalidTokenException();
+        }
     }
     //Request의 Header에서 token 값을 가져옵니다.
     public String resolveToken(HttpServletRequest request){
-        return request.getHeader("X-AUTH-TOKEN");
+        String token = request.getHeader(HEADER);
+
+        if(StringUtils.hasText(token) && token.startsWith(PREFIX)){
+            return token.substring(7);
+        }
+        return null;
     }
 
     //토큰의 유효성 + 만료일자 확인
-    public boolean validateToken(String jwtToken) {
-        try {
-            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
-            return !claims.getBody().getExpiration().before(new Date());
-        } catch (Exception e) {
-            return false;
-        }
+    public boolean validateToken(String token) {
+        return !getUsername(token)
+                .getExpiration()
+                .before(new Date());
     }
 
     public boolean isRefreshToken(String refreshToken) {
         try {
             Claims claims = Jwts.parser()
-                    .setSigningKey(secretKey)
+                    .setSigningKey(SECRET_KEY)
                     .parseClaimsJws(refreshToken)
                     .getBody();
+
             return claims.get("type").equals("refresh") && claims.getExpiration().before(new Date());
         } catch (MalformedJwtException | UnsupportedJwtException e) {
             throw new IncorrectTokenException();
