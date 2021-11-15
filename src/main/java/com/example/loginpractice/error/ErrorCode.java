27de6335af79ebbf471cdacbf7f66dda16385d3f
이정윤 <dlwjddbs14@naver.com>
2021-11-15 14:24:04 +0900

    [UPDATE] error 코드 추가

--- src/main/java/com/example/loginpractice/error/ErrorCode.java
+++ src/main/java/com/example/loginpractice/error/ErrorCode.java
@@ -15,8 +15,11 @@ public enum ErrorCode {
     REFRESH_TOKEN_NOT_FOUND(404, "Refresh Token Not Found"),
 
     //회원
-    MEMBER_NAME_ALREADY_EXISTS(409, "Member Name Already Exists"),
-    MEMBER_EMAIL_ALREADY_EXISTS(409, "Member Email Already Exists"),
+    USER_NAME_ALREADY_EXISTS(409, "User Name Already Exists"),
+    USER_EMAIL_ALREADY_EXISTS(409, "User Email Already Exists"),
+    USER_NOT_FOUND(404, "User Not Found"),
+    USER_USERNAME_ALREADY_EXISTS(409, "User Already Exists"),
+    INVALID_PASSWORD(401, "Invalid Password"),
 
     EMAIL_NOT_CERTIFIED(401, "Email Not Certified"),
     SEND_MESSAGE_FAILED(500, "Send Message Failed"),
