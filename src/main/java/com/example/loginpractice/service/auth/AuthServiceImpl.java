5ec340c10b9eba5209797bc6bb76a64fd6cb347c
이정윤 <dlwjddbs14@naver.com>
2021-11-15 14:14:29 +0900

    [UPDATE] auth 서비스

--- src/main/java/com/example/loginpractice/service/auth/AuthServiceImpl.java
+++ src/main/java/com/example/loginpractice/service/auth/AuthServiceImpl.java
@@ -3,12 +3,17 @@ package com.example.loginpractice.service.auth;
 import com.example.loginpractice.entity.certification.Certification;
 import com.example.loginpractice.entity.certification.CertificationRepository;
 import com.example.loginpractice.entity.certification.Certified;
+import com.example.loginpractice.entity.refreshToken.RefreshToken;
+import com.example.loginpractice.entity.refreshToken.RefreshTokenRepository;
 import com.example.loginpractice.entity.user.User;
 import com.example.loginpractice.entity.user.UserRepository;
 import com.example.loginpractice.exception.*;
 import com.example.loginpractice.payload.request.EmailRequest;
 import com.example.loginpractice.payload.request.EmailVerifiedRequest;
+import com.example.loginpractice.payload.request.LoginRequest;
 import com.example.loginpractice.payload.request.RegisterRequest;
+import com.example.loginpractice.payload.response.TokenResponse;
+import com.example.loginpractice.security.jwt.JwtTokenProvider;
 import com.example.loginpractice.service.mail.MailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,10 +25,14 @@ import javax.transaction.Transactional;
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
+    private long REFRESH_TOKEN_VALID_TIME = 60 * 60 * 24 * 7 * 1000L; //1주
+
     private final MailService mailService;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final CertificationRepository certificationRepository;
+    private final JwtTokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     @Override
     public void sendEmail(EmailRequest request){
@@ -44,7 +53,7 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     @Transactional
-    public String register(RegisterRequest request) {
+    public void register(RegisterRequest request) {
         if (userRepository.findByEmail(request.getName()).isPresent())
             throw new UserNameAlreadyExistsException();
 
@@ -58,7 +67,49 @@ public class AuthServiceImpl implements AuthService {
                     .password(passwordEncoder.encode(request.getPassword()))
                     .build());
         } else throw new EmailNotCertifiedException();
-
-        return "회원가입 성공";
     }
+
+    @Override
+    @Transactional
+    public TokenResponse login(LoginRequest request){
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(UserNotFoundException::new);
+
+        if(!passwordEncoder.matches(request.getPassword(), user.getPassword()))
+            throw new InvalidPasswordException();
+
+        return createToken(request.getEmail());
+    }
+
+    @Override
+    @Transactional
+    public TokenResponse reissue(String refreshToken) {
+        if(!tokenProvider.isRefreshToken(refreshToken)) {
+            throw new InvalidTokenException();
+        }
+
+        RefreshToken newRefreshToken = refreshTokenRepository.findByRefreshToken(refreshToken)
+                .map(refresh -> refreshTokenRepository.save(
+                        refresh.update(REFRESH_TOKEN_VALID_TIME)
+                ))
+                .orElseThrow(RefreshTokenNotFoundException::new);
+
+        return new TokenResponse(tokenProvider.createJwtAccessToken(newRefreshToken.getUsername()), refreshToken);
+    }
+
+    @Transactional
+    public TokenResponse createToken(String username) {
+        String accessToken = tokenProvider.createJwtAccessToken(username);
+        String refreshToken = tokenProvider.createJwtRefreshToken(username);
+
+        refreshTokenRepository.save(
+                RefreshToken.builder()
+                        .username(username)
+                        .refreshToken(refreshToken)
+                        .refreshTokenExpiration(REFRESH_TOKEN_VALID_TIME)
+                        .build());
+
+        return new TokenResponse(accessToken, refreshToken);
+    }
+
 }
