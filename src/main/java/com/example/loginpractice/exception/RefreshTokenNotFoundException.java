53eab6e2b571be020d9fad3fce726cc28a4646fa
이정윤 <dlwjddbs14@naver.com>
2021-11-15 14:24:49 +0900

    [UPDATE] Exception 추가

--- src/main/java/com/example/loginpractice/exception/RefreshTokenNotFoundException.java
+++ src/main/java/com/example/loginpractice/exception/RefreshTokenNotFoundException.java
@@ -1,2 +1,10 @@
-package com.example.loginpractice.exception;public class RefreshTokenNotFoundException {
+package com.example.loginpractice.exception;
+
+import com.example.loginpractice.error.ErrorCode;
+import com.example.loginpractice.error.exception.BusinessException;
+
+public class RefreshTokenNotFoundException extends BusinessException {
+    public RefreshTokenNotFoundException(){
+        super(ErrorCode.REFRESH_TOKEN_NOT_FOUND);
+    }
 }
