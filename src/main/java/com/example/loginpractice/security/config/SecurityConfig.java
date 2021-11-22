912e890f145208bc0d80ae6a5a80b5e7d67d461c
이정윤 <dlwjddbs14@naver.com>
2021-11-22 10:14:50 +0900

    [UPDATE] HttpMethod 추가, swagger-ui 사용권한 허가

--- src/main/java/com/example/loginpractice/security/config/SecurityConfig.java
+++ src/main/java/com/example/loginpractice/security/config/SecurityConfig.java
@@ -1,20 +1,19 @@
 package com.example.loginpractice.security.config;
 
 import com.example.loginpractice.error.ExceptionHandlerFilter;
-import com.example.loginpractice.security.jwt.JwtTokenFilter;
+
 import com.example.loginpractice.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -28,13 +27,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder(){
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
-    // authenticationManager를 Bean 등록합니다.
-
-   /* @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }*/
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -48,15 +40,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .and()
                 .authorizeRequests() //요청에 대한 사용권한 체크
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers("/swagger-ui.html", "/swagger-ui/**").permitAll()
 
-                .antMatchers("/send").permitAll()
-                .antMatchers("/email").permitAll()
-                .antMatchers("/register").permitAll()
-                .antMatchers("/login").permitAll()
-                .antMatchers("/reissue").permitAll()
+                .antMatchers(HttpMethod.POST,"/send").permitAll()
+                .antMatchers(HttpMethod.POST,"/email").permitAll()
+                .antMatchers(HttpMethod.POST,"/register").permitAll()
+                .antMatchers(HttpMethod.POST,"/login").permitAll()
+                .antMatchers(HttpMethod.PUT,"/reissue").permitAll()
                 .anyRequest().authenticated()
 
-                //.antMatchers("/**").permitAll() //그 외 나머지 요청은 누구나 접근 가능
                 .and()
                 .apply(new FilterConfig(jwtTokenProvider, exceptionHandlerFilter));
     }
