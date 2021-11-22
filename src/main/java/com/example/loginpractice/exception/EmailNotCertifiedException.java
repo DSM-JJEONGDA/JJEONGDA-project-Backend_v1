00d3ad7aefd24ea31b394feffb092baf47d159f9
이정윤 <dlwjddbs14@naver.com>
2021-11-22 10:13:28 +0900

    [UPDATE] Exception 로직 수정

--- src/main/java/com/example/loginpractice/exception/EmailNotCertifiedException.java
+++ src/main/java/com/example/loginpractice/exception/EmailNotCertifiedException.java
@@ -4,7 +4,10 @@ import com.example.loginpractice.error.ErrorCode;
 import com.example.loginpractice.error.exception.BusinessException;
 
 public class EmailNotCertifiedException extends BusinessException {
-    public EmailNotCertifiedException(){
+    public static BusinessException EXCEPTION =
+            new EmailNotCertifiedException();
+
+    private EmailNotCertifiedException(){
         super(ErrorCode.EMAIL_NOT_CERTIFIED);
     }
 }
