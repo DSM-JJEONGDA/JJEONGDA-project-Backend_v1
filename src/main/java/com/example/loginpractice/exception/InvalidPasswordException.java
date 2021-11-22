89a25df28c2df26656ace40fd9a42f95f3b938c3
이정윤 <dlwjddbs14@naver.com>
2021-11-22 10:12:54 +0900

    [UPDATE] Exception 로직 수정

--- src/main/java/com/example/loginpractice/exception/InvalidPasswordException.java
+++ src/main/java/com/example/loginpractice/exception/InvalidPasswordException.java
@@ -4,7 +4,10 @@ import com.example.loginpractice.error.ErrorCode;
 import com.example.loginpractice.error.exception.BusinessException;
 
 public class InvalidPasswordException extends BusinessException {
-    public InvalidPasswordException(){
+    public static BusinessException EXCEPTION =
+            new InvalidPasswordException();
+
+    private InvalidPasswordException(){
         super(ErrorCode.INVALID_PASSWORD);
     }
 }
