154e17406083ca6ae9ef06d159834454430c9668
이정윤 <dlwjddbs14@naver.com>
2021-11-28 11:07:18 +0900

    ⚡ :: UserDetails 상속 삭제

--- src/main/java/com/example/loginpractice/entity/user/User.java
+++ src/main/java/com/example/loginpractice/entity/user/User.java
@@ -1,16 +1,8 @@
 package com.example.loginpractice.entity.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import lombok.*;
 
 import javax.persistence.*;
-import java.util.Collection;
-import java.util.Collections;
 
 @Getter
 @Entity
@@ -18,7 +10,7 @@ import java.util.Collections;
 @AllArgsConstructor
 @Builder
 @Table(name = "user")
-public class User implements UserDetails {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -32,42 +24,7 @@ public class User implements UserDetails {
     @Column(length = 200, nullable = false)
     private String password;
 
+    @Setter
     @Enumerated(EnumType.STRING)
-    private Role role;
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(new SimpleGrantedAuthority(role.name()));
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-
+    private Authority authority;
 }
