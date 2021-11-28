0cba9f735a487ae9748811e2fe7cb1a7842b9a67
이정윤 <dlwjddbs14@naver.com>
2021-11-28 11:07:51 +0900

    ⚡ :: UserDetailsService 수정

--- src/main/java/com/example/loginpractice/security/auth/CustomUserDetailService.java
+++ src/main/java/com/example/loginpractice/security/auth/CustomUserDetailService.java
@@ -8,15 +8,19 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    @Transactional
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByEmail(username)
+                .map(CustomUserDetails::new)
                 .orElseThrow(UserNotFoundException::new);
 
     }
