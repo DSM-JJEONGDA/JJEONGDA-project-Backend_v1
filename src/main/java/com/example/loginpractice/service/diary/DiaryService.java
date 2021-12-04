d3cf2cc9bda555d4caf8e616b3574435b3066439
이정윤 <dlwjddbs14@naver.com>
2021-12-04 19:54:47 +0900

    ⚡ :: 서비스 인터페이스 수정

--- src/main/java/com/example/loginpractice/service/diary/DiaryService.java
+++ src/main/java/com/example/loginpractice/service/diary/DiaryService.java
@@ -1,9 +1,13 @@
 package com.example.loginpractice.service.diary;
 
+import com.example.loginpractice.entity.diary.DiaryEntity;
 import com.example.loginpractice.payload.request.DiaryRequest;
+
+import java.util.List;
 
 public interface DiaryService {
     void create(DiaryRequest request);
     void update(Long id, DiaryRequest request);
     void delete(Long id);
+    List<DiaryEntity> getAllDiary();
 }
